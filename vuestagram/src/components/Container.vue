c45c3daf9189d31747fcd4e5128904c83c57a178
KwonSeongHyeok <aiqksiwk@naver.com>
2024-11-26 23:29:52 +0900

    instagram 만들기

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name : 'ContainerComponent',
+    components : {
+        Post : Post,
+    },
+};
+</script>
+
+<style>
+</style>
